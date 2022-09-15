0d95e4fd2baf40ba43adacdae5d1367f4e8b2cb6
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-15 10:35:07 +0200

    style: add style section

--- src/Components/UseState/index.jsx
+++ src/Components/UseState/index.jsx
@@ -10,11 +10,15 @@ const ExempleUseState = () => {
     setCount((prevState) => prevState + 1)
   }
   return ( 
-    <div className="m-10 justify-center items-center flex">
-      <h1 className="font-bold mr-5 bg-red-300 px-3 py-2 rounded">{count}</h1>
-      {/* When the user clicks, we call incrementCount, and thats will render setCount with a new value. React will then re-render the Example component, passing the new count value to it. */}
-      <button className="p-2 bg-slate-500 text-white rounded hover:bg-slate-800" onClick={incrementCount}>Increment</button>
-    </div>
+    <section className="border border-slate-500 rounded p-2">
+      <h1 className="font-bold m-2">UseState Example</h1>
+      <div className="mb-10 justify-center items-center flex">
+        <h2 className="font-bold mr-5 bg-red-300 px-3 py-2 rounded">{count}</h2>
+        {/* When the user clicks, we call incrementCount, and thats will render setCount with a new value. React will then re-render the Example component, passing the new count value to it. */}
+        <button className="p-2 bg-slate-500 text-white rounded hover:bg-slate-800" onClick={incrementCount}>Increment</button>
+      </div>
+    </section>
+    
    );
 }
  
